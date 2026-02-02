e3d6eb8d31a14a5c28b2c78d50daf04228130fe1
engesin <engesin@gmail.com>
2026-02-02 20:56:16 +0300

    Fix import: save_statistics_csv renamed to rows_to_csv_string

--- custom_components/shelly_integrator/utils/__init__.py
+++ custom_components/shelly_integrator/utils/__init__.py
@@ -5,7 +5,7 @@ Contains pure helper functions without side effects.
 from .csv_converter import (
     parse_shelly_csv,
     convert_to_statistics_format,
-    save_statistics_csv,
+    rows_to_csv_string,
     convert_channel_csv,
 )
 from .http import fetch_csv_from_gateway
@@ -13,7 +13,7 @@ from .http import fetch_csv_from_gateway
 __all__ = [
     "parse_shelly_csv",
     "convert_to_statistics_format",
-    "save_statistics_csv",
+    "rows_to_csv_string",
     "convert_channel_csv",
     "fetch_csv_from_gateway",
 ]
